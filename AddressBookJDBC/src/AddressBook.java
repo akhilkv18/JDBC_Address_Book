8dedae837ddf1305911884f0e50b0e9f1fd83e61
AkhilMcaKuc <akhilkvmcakuc@gmail.com>
2024-01-01 21:54:17 +0530

    Created a method AddressBook() which include the interface setups related with the actions

--- AddressBookJDBC/src/AddressBook.java
+++ AddressBookJDBC/src/AddressBook.java
@@ -10,6 +10,90 @@ public class AddressBook {
 	    private static final String PASS = "akhilmysql";
 
 	    private Connection conn = null;
+	    
+	    public AddressBook(){
+	        try {
+	            Class.forName(JDBC_DRIVER);
+	            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	        } catch (ClassNotFoundException | SQLException e) {
+	            showError("Database Connection Error", "Failed to connect to the database.");
+	            System.exit(1);
+	        }
+
+	        JFrame frame = new JFrame("Address Book");
+	        frame.setSize(500, 300);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	        JPanel panel = new JPanel();
+
+	        JLabel nameLabel = new JLabel("Name:");
+	        JTextField nameField = new JTextField(20);
+
+	        JLabel phoneLabel = new JLabel("Phone:");
+	        JTextField phoneField = new JTextField(20);
+
+	        JLabel emailLabel = new JLabel("Email:");
+	        JTextField emailField = new JTextField(20);
+
+	        JRadioButton mobileByEmail = new JRadioButton("Update Mobile by Email");
+	        JRadioButton emailByPhone = new JRadioButton("Update Email by Phone");
+
+	        ButtonGroup group = new ButtonGroup();
+	        group.add(mobileByEmail);
+	        group.add(emailByPhone);
+	        
+	        
+	        JButton addButton = new JButton("Add Contact");
+	        addButton.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                insertContact(nameField.getText(), phoneField.getText(), emailField.getText());
+	            }
+
+	        });
+
+	        JButton updateButton = new JButton("Update Contact");
+	        updateButton.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                if (mobileByEmail.isSelected()) {
+	                    updateMobileByEmail(nameField.getText(), emailField.getText(), phoneField.getText());
+	                } else if (emailByPhone.isSelected()) {
+	                    updateEmailByPhone(nameField.getText(), phoneField.getText(), emailField.getText());
+	                } else {
+	                    JOptionPane.showMessageDialog(null, "Please select an update option!");
+	                }
+	            }
+	        });
+	        
+	        JButton deleteButton = new JButton("Delete Contact");
+	        deleteButton.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                deleteContact(nameField.getText());
+	            }
+	        });
+
+	        panel.add(nameLabel);
+	        panel.add(nameField);
+	        panel.add(phoneLabel);
+	        panel.add(phoneField);
+	        panel.add(emailLabel);
+	        panel.add(emailField);
+	        panel.add(addButton);
+	        panel.add(mobileByEmail);
+	        panel.add(emailByPhone);
+	        panel.add(updateButton);
+	        
+	        panel.add(deleteButton);
+	        
+	        
+
+	        frame.add(panel);
+	        frame.setVisible(true);
+	    }
+	  
+
 
 	
 }
