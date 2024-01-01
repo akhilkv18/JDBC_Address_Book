b9eae77f1519cca27e8ad162298555273a3028a0
AkhilMcaKuc <akhilkvmcakuc@gmail.com>
2024-01-01 21:42:19 +0530

    setup the configuration with mysql.We already created a database address_book and a table contacts with fields-id,name,phone,email in it

--- AddressBookJDBC/src/AddressBook.java
+++ AddressBookJDBC/src/AddressBook.java
@@ -4,5 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 public class AddressBook {
+	    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	    private static final String DB_URL = "jdbc:mysql://localhost/address_book";
+	    private static final String USER = "root";
+	    private static final String PASS = "akhilmysql";
 
+	    private Connection conn = null;
+
+	
 }
